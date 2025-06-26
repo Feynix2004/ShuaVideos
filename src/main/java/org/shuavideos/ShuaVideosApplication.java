33a5297a88b7463b5331f20e69950c4501046785
stonepig <1048218204@qq.com>
2025-06-26 17:38:07 +0800

    refactor(package): rename base package from org.feynix to org.shuavideos

--- src/main/java/org/shuavideos/ShuaVideosApplication.java
+++ src/main/java/org/shuavideos/ShuaVideosApplication.java
@@ -1,4 +1,4 @@
-package org.feynix;
+package org.shuavideos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/shuavideos/ShuaVideosApplicationTests.java
+++ src/test/java/org/shuavideos/ShuaVideosApplicationTests.java
@@ -1,4 +1,4 @@
-package org.feynix;
+package org.shuavideos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
